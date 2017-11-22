4d4542ecaebc2d3bbb62d4c8b18b725e7e64d56c
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-22 20:42:46 +0000

    Added route and navigation links.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import {PostajobComponent} from "./components/postajob/postajob.component";
 
 const routes: Routes = [
+  {path: 'post-a-job', component: PostajobComponent},
   {path: '', component: HomeComponent, pathMatch: 'full'}
 ];
 
